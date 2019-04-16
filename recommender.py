a58db8d0d7db0ce6785312f21d76743259bb01dc
Santiago Bermúdez <sabefo@Santiagos-MacBook-Pro.local>
2019-04-16 14:23:06 +0200

    refactor code

--- recommender.py
+++ recommender.py
@@ -2,11 +2,6 @@ import numpy as np
 from lightfm.datasets import fetch_movielens
 from lightfm import LightFM
 
-data = fetch_movielens(min_rating = 4.0)
-
-model = LightFM(loss = 'warp')
-
-model.fit(data['train'], epochs=30, num_threads=2)
 
 def sample_recommendation(model, data, user_ids):
     n_users, n_items = data['train'].shape
@@ -24,5 +19,10 @@ def sample_recommendation(model, data, user_ids):
 
         for x in top_items[:5]:
             print("\t %s" % x)
+
+
+data = fetch_movielens(min_rating=4.0)
+model = LightFM(loss='warp')
+model.fit(data['train'], epochs=30, num_threads=2)
 
 sample_recommendation(model, data, [3, 20, 451])
